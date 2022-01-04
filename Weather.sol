b161eaec29d287f9857f03378f19edc7af8d3288
sgnovo8 <88598099+sgnovo8@users.noreply.github.com>
2022-01-04 14:13:07 -0500

    Create Weather.sol

---
+++ Weather.sol
@@ -0,0 +1,32 @@
+pragma solidity 0.8.0;
+
+contract Weather {
+
+//current state
+    uint windSpeed;
+    uint precipPercent = 50;
+    uint temperature = 30;
+    uint humidity;
+    string season = "summer";
+
+//potential behavior changes
+    function windSpdKph (uint256 _kph) public  {
+        windSpeed = _kph;
+    }
+
+    function getWindSpeed () public view returns(uint _kph){
+        return windSpeed + 10;
+    }
+
+    function precip (uint _percentagePrecip) public {
+        precipPercent = _percentagePrecip;
+    }
+
+    function getPrecip() public view returns(uint256 _percentagePrecip){
+        return precipPercent - 23;
+    }
+
+    function getSeason() public view returns (string memory){
+        return season;
+    }
+}
